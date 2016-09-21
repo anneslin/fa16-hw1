cbbf10221079cb66ff3e227818aa393485b7c3cb
Anne Lin <miss.annelin@gmail.com>
2016-09-21 15:55:45 -0700

    submitting homework 1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,11 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map!{|element| element.to_i + 2}
+    a.select!{|i| i % 2 == 0}
+    a.uniq!
+    a.reject!{|i| i > 10}
+    a.inject{|sum, i| sum + i}
   end
 end
 
